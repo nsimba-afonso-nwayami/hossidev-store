69e28477bfecbfc87445a160c3b058ff9278227e
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2026-04-08 11:53:45 +0100

    Título adicionado em carrinho

--- src/pages/site/Carrinho.jsx
+++ src/pages/site/Carrinho.jsx
@@ -22,6 +22,7 @@ export default function Carrinho() {
 
   return (
     <>
+      <title>Carrinho | Hossidev Dev</title>
       <section className="w-full bg-neutral-100 min-h-screen pt-32 md:pt-40 pb-20">
         <div className="max-w-7xl mx-auto px-6">
           
